0bbc07ad8704f2c0d2bfc38e8a2438daf8e20fca
Andrew Stanton <Refefer@gmail.com>
2014-01-29 11:55:13 -0800

    Copyrights

--- src/main/scala/com/impact/utils/distribution/DistUtils.scala
+++ src/main/scala/com/impact/utils/distribution/DistUtils.scala
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2013 Impact Computing Corporation
+** Copyright (c) 2014 Impact Computing Corporation
 **
 ** Licensed under the Apache License, Version 2.0 (the "License")
 ** you may not use this file except in compliance with the License.
